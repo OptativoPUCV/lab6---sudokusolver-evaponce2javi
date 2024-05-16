b45c0ee1061febd036a6994b6dc648de09cde938
Your Name <you@example.com>
2024-05-16 03:04:31 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -1,69 +1,92 @@
+#include "list.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "list.h"
 
+typedef struct {
+  int sudo[9][9];
+} Node;
 
-typedef struct{
-   int sudo[9][9];
-}Node;
-
-Node* createNode(){
-  Node* n=(Node*) malloc(sizeof(Node));
+Node *createNode() {
+  Node *n = (Node *)malloc(sizeof(Node));
   return n;
 }
 
-Node* copy(Node* n){
-    Node* new=(Node*) malloc(sizeof(Node));
-    *new = *n;
-    return new;
+Node *copy(Node *n) {
+  Node *new = (Node *)malloc(sizeof(Node));
+  *new = *n;
+  return new;
 }
 
-Node* read_file (char* file_name){
-  Node* n = createNode();
-  FILE* file = fopen (file_name, "r");
-  int i,j;
-  for(i=0;i<9;i++){
-       for(j=0;j<9;j++){
-          if(!fscanf (file, "%d", &n->sudo[i][j]))
-            printf("failed to read data!");
-       }
+Node *read_file(char *file_name) {
+  Node *n = createNode();
+  FILE *file = fopen(file_name, "r");
+  int i, j;
+  for (i = 0; i < 9; i++) {
+    for (j = 0; j < 9; j++) {
+      if (!fscanf(file, "%d", &n->sudo[i][j]))
+        printf("failed to read data!");
+    }
   }
 
-  fclose (file);
+  fclose(file);
   return n;
 }
 
-void print_node(Node* n){
-    int i,j;
-    for(i=0;i<9;i++){
-       for(j=0;j<9;j++)
-          printf("%d ", n->sudo[i][j]);
-       printf("\n");
-    }
+void print_node(Node *n) {
+  int i, j;
+  for (i = 0; i < 9; i++) {
+    for (j = 0; j < 9; j++)
+      printf("%d ", n->sudo[i][j]);
     printf("\n");
+  }
+  printf("\n");
 }
 
-int is_valid(Node* n){
-
-    return 1;
+int recorrerMatriz(Node *n, int a, int b) {
+  int auxSudo = n->sudo[a][b];
+  for (int i = 0; i < 9; i++) {
+    for (int k = 0; k < 9; k++) {
+      if (n->sudo[i][k] == auxSudo)
+        return 0;
+    }
+  }
+  return 1;
 }
 
-
-List* get_adj_nodes(Node* n){
-    List* list=createList();
-    return list;
+int is_valid(Node *n) {
+  int auxSudo = n->sudo[0][0];
+  /*
+ No se repitan números en las filas
+ No se repitan números en las columnas
+ No se repitan números en las submatrices de 3x3
+ // If numero is in sitio, return cero
+ // if no está, return 1
+ */
+  for (int i = 0; i < 9; i++) {
+    for (int k = 0; k < 9; k++) {
+      if (recorrerMatriz(n, i, k) == 0)
+        return 0;
+    }
+  }
+  return 1;
 }
 
-
-int is_final(Node* n){
-    return 0;
+List *get_adj_nodes(Node *n) {
+  List *list = createList();
+  for (int i = 0; i < 9; i++) {
+    for (int k = 0; k < 9; k++) {
+      if (n->sudo[i][k] == 0) // vacio kuak
+      {
+        Node *newN = copy(n);
+      }
+    }
+  }
+  return list;
 }
 
-Node* DFS(Node* initial, int* cont){
-  return NULL;
-}
+int is_final(Node *n) { return 0; }
 
-
+Node *DFS(Node *initial, int *cont) { return NULL; }
 
 /*
 int main( int argc, char *argv[] ){
